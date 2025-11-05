b0650b2db9c4809658937a582cdf45b08bdd937b
zhangbofei <zhangbofei@miliantech.com>
2025-11-05 21:44:31 +0800

    hardhat deploy set network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL
+const PRIMARY_KEY = process.env.PRIMARY_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    url: "",
-    accounts: []
+    url: SEPOLIA_URL,
+    accounts: [PRIMARY_KEY]
   }
 };
