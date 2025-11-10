631d25dc0dc360d4094e9c5800abff051398f0e5
zhangbofei <zhangbofei@jiuzhou.com>
2025-11-10 10:13:46 +0800

    edit conf

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config
+require("@chainlink/env-enc").config();
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL
 const PRIMARY_KEY = process.env.PRIMARY_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    url: SEPOLIA_URL,
-    accounts: [PRIMARY_KEY]
-  }
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIMARY_KEY]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
